006763ba6657e7d910f09e70f0f7eee42566ae33
Neville Jos <josneville@yahoo.com>
2015-02-08 02:39:29 -0600

    access control fixed

--- server.js
+++ server.js
@@ -9,7 +9,24 @@ app.set('port', process.env.PORT || '5000');
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 mongoose.connect("mongodb://josneville:bananaman@ds041861.mongolab.com:41861/heroku_app33821378");
 
 var gigSchema = new Schema({
