32cacdd5f4d70dc492bee42fbcdfdab34ca0de70
Neville Jos <josneville@yahoo.com>
2015-02-11 15:22:24 -0600

    server done for the most part

--- server.js
+++ server.js
@@ -28,43 +28,145 @@ app.use(function (req, res, next) {
     next();
 });
 mongoose.connect("mongodb://josneville:bananaman@ds041861.mongolab.com:41861/heroku_app33821378");
-
+//mongoose.connect("localhost");
 var gigSchema = new Schema({
-	userid: {type: String, required: true},
+  userid: {type: String, required: true},
+  name: {type: String, required: true},
 	position: {type: String, required: true},
 	rate: {type: Number, required: true},
 	date: {type: Date, required: true},
-	name: {type: String, required: true},
-  description: {type: String, required: true}
+  description: {type: String, required: true},
+  interested: {type: Array, default: []}
 }, {versionKey:false});
+
+var userSchema = new Schema({
+  userid: {type: String, required: true},
+  name: {type: String, required: true},
+  email: {type: String, required: true},
+  phone: {type: Number},
+  caption: {type: String}
+}, {versionKey: false});
 
 
 var Gig = mongoose.model('Gig', gigSchema);
+var User = mongoose.model('FbUser', userSchema);
 
-app.post('/api/gig/all', function(req, res){
-	Gig.find({}, function(err, gigs){
-		res.status(200).send(gigs);
-	});
+app.post('/api/users/new', function(req, res){
+  var postID = req.body.userID;
+  var postName = req.body.name;
+  var postEmail = req.body.email;
+  User.findOne({userid: postID}, function(err, user){
+    if (err || user == null){
+      var newUser = new User({
+        userid: postID,
+        name: postName,
+        email: postEmail
+      })
+      newUser.save(function(err, nUser){
+        if (err){
+          res.status(400).send("Unknown Error");
+          return;
+        }
+        res.status(200).send("New User Created");
+      })
+    }
+    else{
+      res.status(200).send("User found");
+    }
+  })
 });
 
-app.post('/api/gig/new', function(req, res){
-	console.log(req.body);
-	var newGig = new Gig({
-		userid: req.body.userID,
-		position: req.body.position,
-		rate: req.body.rate,
-		date: req.body.date,
-		name: req.body.name.
-    description: req.body.description
-	});
-	newGig.save(function(err, gig){
-		if (err) {
-			console.log(err);
-			res.status(400).send("Unknown Error");
-			return;
-		}
-		res.status(200).send("Success");
-	});
+app.post('/api/users/get', function(req, res){
+  var postID = req.body.userID;
+  User.findOne({userid: postID}, function(err, user){
+    if (err || user == null){
+      res.status(400).send("Unknown Error");
+      return;
+    }
+    res.status(200).send(user);
+  });
+});
+
+app.post('/api/users/update', function(req, res){
+  var postCaption = req.body.caption;
+  var postPhone = req.body.phone;
+  var postID = req.body.userID;
+  User.update({userid: postID}, {caption: postCatpion, phone: postPhone}, {}, function(err, numUpdated){
+    if (err){
+      res.status(400).send("Fail");
+      return;
+    }
+    res.status(200).send("Success");
+  });
+});
+
+app.post('/api/gigs/feed', function(req, res){
+  var postID = req.body.userID;
+  console.log(postID);
+  Gig.aggregate([
+      {$match: {userid: {$ne: postID}}},
+      {$project: {
+        _id: 1,
+        name: 1,
+        position: 1,
+        rate: 1,
+        date: 1,
+        description: 1
+      }}
+    ], function(err, gigs){
+      if (err) {
+        res.status(400).send("Unknown Error");
+        return;
+      }
+	  console.log(gigs);
+      res.status(200).send(gigs);
+  })
+});
+
+app.post('/api/gigs/personal', function(req, res){
+  var postID = req.body.userID;
+  Gig.aggregate([
+      {$match: {userid: postID}},
+      {$project: {
+        _id: 1,
+        name: 1,
+        position: 1,
+        rate: 1,
+        date: 1,
+        description: 1,
+        interested: 1
+      }}
+    ], function(err, gigs){
+      if (err) {
+        res.status(400).send("Unknown Error");
+        return;
+      }
+      res.status(200).send(gigs);
+  })
+});
+
+app.post('/api/gigs/new', function(req, res){
+  var postID = req.body.userID;
+  var postName = req.body.name;
+  var postPosition = req.body.position;
+  var postRate = req.body.rate;
+  var postDescription = req.body.description;
+  var postDate = req.body.date;
+  var newGig = new Gig({
+    userid: postID,
+    name: postName,
+    position: postPosition,
+    rate: postRate,
+    description: postDescription,
+    date: postDate
+  });
+  newGig.save(function(err, nU){
+    if(err){
+      res.status(400).send("Unknown Error");
+      return;
+    }
+    res.status(200).send("Sucess");
+  })
 });
 
 app.listen(app.get('port'));
